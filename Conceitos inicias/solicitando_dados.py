04bd3788093351bb66875e4654b6edb87710328f
cstawdl10 <costawendel2018@outlook.com>
2025-02-13 19:59:39 +0000

    Salvando

---
+++ Conceitos inicias/solicitando_dados.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("clear")
+
+# Solicitando dados.
+idade = int(input("Digite sua idade: "))
+
+# Verificando (Processamento)
+if idade < 18:
+    print("Acesso negado!")
+
+
+# Exibindo dados (Saída)
+print("==Fim==")
---
+++ estruturas_condicionais/condicionais_simples.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("clear")
+
+# Solicitando dados.
+valor = int(input("Digite sua valor: "))
+
+# Verificando (Processamento)
+if valor < 10:
+    print("NÃO É MAIOR QUE 10!")
+else:
+    print("É MAIOR QUE 10!")
+
+
+# Exibindo dados (Saída)
+print("F")
