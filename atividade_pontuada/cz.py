684a2d2cc49bd8ba0ef5bce2c35dc2581224abb6
cstawdl10 <costawendel2018@outlook.com>
2025-05-27 17:05:52 -0300

    salvar

--- atividade_pontuada/cz.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("clear")
-
-abel = input("Digite 'S' ou 'sim' se você aceita namorar comigo: ").lower()
-if abel == "s" or "sim":
-    print("Te amo meu amor!!!!!!!!!!!")
-else:
-    print("Vá tomar no seu cu vagabunda!!!!!!!")
--- laço_repetição/laco_for.py
+++
@@ -1,7 +0,0 @@
-import os 
-os.system ("clear")
-
-
-print("Com laço de repetição.")
-for i in range(1,12):
-    print(f"Valor da variável i : {i}")
